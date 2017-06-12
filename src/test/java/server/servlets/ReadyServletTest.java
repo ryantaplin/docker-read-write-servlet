9bd85506ee1337dd8a2afb836cc9352c075e9378
ryan <ryan.taplin170596@gmail.com>
2017-06-12 22:33:11 +0100

    Started added yatspec tests

--- src/test/java/server/servlets/ReadyServletTest.java
+++ src/test/java/server/servlets/ReadyServletTest.java
@@ -1,27 +1,30 @@
 package server.servlets;
 
+import com.googlecode.yatspec.junit.SpecRunner;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.eclipse.jetty.http.HttpStatus;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import server.BasicServer;
-import server.handlers.HandlerBuilder;
 import server.handlers.WriteHandlerBuilder;
 
 import java.io.IOException;
 
-import static org.eclipse.jetty.http.HttpStatus.*;
+import static org.eclipse.jetty.http.HttpStatus.OK_200;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static utils.Settings.getServerSettings;
 
+
+@RunWith(SpecRunner.class)
 public class ReadyServletTest {
 
     private BasicServer server;
+    private CloseableHttpResponse response;
 
     @Before
     public void setUp() throws Exception {
@@ -36,18 +39,30 @@ public class ReadyServletTest {
     }
 
     @Test
-    public void readyServletReturnsOKWhenHappy() throws Exception {
-        CloseableHttpResponse response = getRequestTo("http://localhost:8080/ready");
+    public void shouldReturnOK() throws Exception {
+        whenWeHitEndpoint("ready");
+        thenTheResponseCodeIs(200);
+        andTheResposeBodyIs("OK");
+    }
 
-        assertThat("Response Code", getResponseCode(response), is(OK_200));
-        assertThat("Content Type", getResponsBody(response), is("OK"));
+    private void thenTheResponseCodeIs(int responseCode) {
+        assertThat("Response Code", getResponseCode(response), is(responseCode));
+    }
+
+    private void andTheResposeBodyIs(String responseBody) throws IOException {
+        assertThat("Content Type", getResponseBody(response), is(responseBody));
+    }
+
+    private void whenWeHitEndpoint(String endpoint) throws IOException {
+        String url = "http://localhost:8080/" + endpoint;
+        response = getRequestTo(url);
     }
 
     private int getResponseCode(CloseableHttpResponse response) {
         return response.getStatusLine().getStatusCode();
     }
 
-    private String getResponsBody(CloseableHttpResponse response) throws IOException {
+    private String getResponseBody(CloseableHttpResponse response) throws IOException {
         return EntityUtils.toString(response.getEntity());
     }
 
